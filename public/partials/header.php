3b1c315218cde38dc613f7788e8403e9bb1fad6a
TaniyanR <toolhouse@24.am>
2026-08-01 08:40:02 +0900

    正式名称をPinkClub-DLへ統一

--- public/partials/header.php
+++ public/partials/header.php
@@ -32,7 +32,7 @@ if ($siteName === '') {
     $siteName = trim($safeTextSetting('site.title', ''));
 }
 if ($siteName === '') {
-    $siteName = 'PinkClub-FD';
+    $siteName = 'PinkClub-DL';
 }
 
 $tagline = trim($safeTextSetting('site.tagline', ''));
