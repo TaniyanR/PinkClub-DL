b6b94e14b428c586e67ddba13dc1392f3e22476f
TaniyanR <toolhouse@24.am>
2026-08-01 09:25:47 +0900

    FLと同じ初期管理者設定へ戻す

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,1 +1,4 @@
--- 管理者はセットアップ時に安全な初期パスワードで作成します。
+-- 初期管理者: admin / password
+INSERT INTO admins (username, password_hash)
+VALUES ('admin', '$2y$12$ptQDVasJVLSGY8k1mhmvtOfkp.4EiOtQHxvs4Mux4ZJrh0RjpRIPm')
+ON DUPLICATE KEY UPDATE username = VALUES(username);
